d04e45064411bfc5f60312cf839aa9705ab9e64d
unknown <mobinaghashahi13@gmail.com>
2023-03-03 02:51:44 +0330

    fix bug nullable for some filds

--- database/migrations/2023_03_02_230445_products.php
+++ database/migrations/2023_03_02_230445_products.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string("description",300)->nullable();
-            $table->string("name");
-            $table->integer("count");
-            $table->integer("price");
+            $table->string("name")->nullable();
+            $table->integer("count")->nullable();
+            $table->integer("price")->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
