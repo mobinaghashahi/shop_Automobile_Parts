52504e8ce4482c98ad31a2d2f845ae8094bd9bb4
unknown <mobinaghashahi13@gmail.com>
2023-03-03 03:19:29 +0330

    fix sort fild in table products

--- database/migrations/2023_03_02_230445_products.php
+++ database/migrations/2023_03_02_230445_products.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string("description",300)->nullable();
             $table->string("name");
+            $table->string("description",300)->nullable();
             $table->integer("count");
             $table->integer("price");
             $table->rememberToken();
