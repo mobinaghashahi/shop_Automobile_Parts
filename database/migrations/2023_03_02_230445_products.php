bbb6915b46996090d232c48ba9d0789afdbf111b
unknown <mobinaghashahi13@gmail.com>
2023-03-03 02:55:57 +0330

    fix bug in products table

--- database/migrations/2023_03_02_230445_products.php
+++ database/migrations/2023_03_02_230445_products.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string("description",300)->nullable();
-            $table->string("name")->nullable();
-            $table->integer("count")->nullable();
-            $table->integer("price")->nullable();
+            $table->string("name");
+            $table->integer("count");
+            $table->integer("price");
             $table->rememberToken();
             $table->timestamps();
         });
