6e48155e29db2cdcae1b81ceaa2c387024db496f
unknown <mobinaghashahi13@gmail.com>
2023-03-03 04:11:01 +0330

    add foreign key to products

---
+++ database/migrations/2023_03_03_003746_products.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("description",300)->nullable();
+            $table->integer("count");
+            $table->integer("price");
+
+            $table->unsignedBigInteger('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brand');
+
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('category');
+
+            $table->unsignedBigInteger('carType_id');
+            $table->foreign('carType_id')->references('id')->on('carType');
+
+            $table->unsignedBigInteger('off_id');
+            $table->foreign('off_id')->references('id')->on('off');
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
