aea7d99a7e0d87702d8856c406c03b2043eafcf9
unknown <mobinaghashahi13@gmail.com>
2023-03-04 23:44:14 +0330

    create header

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/RWD.css" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <div class="col-12" style="background-color: #174799">
+        <a href="#">
+            <img src="/logo/instagram.png" width="25" height="25" style="padding: 10px 0px 10px 20px">
+        </a>
+    </div>
+    <div class="col-12">
+        <a href="#">
+            <img class="logo" src="/logo/logo.png">
+        </a>
+        <div style="padding-top: 40px;padding-left: 20px">
+            <div style="display: inline;padding-right: 20px;">
+                <a style="font-size: 12px">محمد مبین آقاشاهی</a>
+            </div>
+            <div style="display: inline;text-align: right;direction: rtl">
+                <input class="inputSearch" type="text" placeholder="جست و جو...">
+            </div>
+        </div>
+    </div>
+    <div class="col-12">
+        <ul class="ulNavigator">
+            <li><a href="#home">خانه</a></li>
+            <li><a href="#news">اخبار</a></li>
+            <li class="dropdown">
+                <a class="dropbtn">محصولات</a>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</header>
+
+
+</body>
+</html>
