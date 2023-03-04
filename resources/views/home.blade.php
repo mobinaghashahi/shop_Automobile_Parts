3454a8500c4b0cec448b6d66f6659547dc1a6ba6
unknown <mobinaghashahi13@gmail.com>
2023-03-05 00:38:48 +0330

    add home page for test header and footer position

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="col-12" style="display: flex;justify-content: center">
+        <img src="/logo/banner.jpg" width="100%" style="">
+    </div>
+@endsection
