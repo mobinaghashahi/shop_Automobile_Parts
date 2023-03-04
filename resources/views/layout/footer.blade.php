479c03bc10bd8e059335388a7e17b6e38b7cafba
unknown <mobinaghashahi13@gmail.com>
2023-03-05 01:12:19 +0330

    clean styles and move all of styles inline to style.css

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,32 +1,32 @@
-<div class="col-12" style="background-color: #174799">
-    <div class="col-12" style="float: right;padding: 20px 0px 20px 0px">
-        <div class="col-3" style="display: flex;justify-content: center">
-            <div style="display: flex;justify-content: center;padding-right: 5px">
-                <a style="margin: auto;color: #e2e8f0">خرید آسان</a>
+<div class="col-12 footer">
+    <div class="col-12 divItemsFooter">
+        <div class="col-3 divItemFooter">
+            <div class="divTextItemsFooter">
+                <a>خرید آسان</a>
             </div>
             <div>
                 <img src="/logo/easy.png" width="50" height="60" >
             </div>
         </div>
-        <div class="col-3" style="display: flex;justify-content: center">
-            <div style="display: flex;justify-content: center;padding-right: 5px">
-                <a style="margin: auto;color: #e2e8f0">خرید امن</a>
+        <div class="col-3 divItemFooter">
+            <div class="divTextItemsFooter">
+                <a>خرید امن</a>
             </div>
             <div>
                 <img src="/logo/shild.png" width="50" height="60" >
             </div>
         </div>
-        <div class="col-3" style="display: flex;justify-content: center">
-            <div style="display: flex;justify-content: center;padding-right: 5px">
-                <a style="margin: auto;color: #e2e8f0">تضمین کیفیت کالاها</a>
+        <div class="col-3 divItemFooter">
+            <div class="divTextItemsFooter">
+                <a>تضمین کیفیت کالاها</a>
             </div>
             <div>
                 <img src="/logo/star.png" width="60" height="60" >
             </div>
         </div>
-        <div class="col-3" style="display: flex;justify-content: center">
-            <div style="display: flex;justify-content: center;padding-right: 5px">
-                <a style="margin: auto;color: #e2e8f0">ارسال به سرار ایران</a>
+        <div class="col-3 divItemFooter">
+            <div class="divTextItemsFooter">
+                <a>ارسال به سرار ایران</a>
             </div>
             <div>
                 <img src="/logo/truck.png" width="90" height="60" >
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,29 +4,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/style.css" rel="stylesheet">
     <link href="/css/RWD.css" rel="stylesheet">
+    <link href="/css/styleLogos.css" rel="stylesheet">
 </head>
 <body>
 
 <header>
-    <div class="col-12" style="background-color: #174799">
+    <div class="col-12 header">
         <a href="#">
-            <img src="/logo/instagram.png" width="25" height="25" style="padding: 10px 0px 10px 20px">
+            <img class="logoInstagram" src="/logo/instagram.png" width="25" height="25">
         </a>
     </div>
     <div class="col-12">
         <a href="#">
             <img class="logo" src="/logo/logo.png">
         </a>
-        <div style="padding-top: 40px;padding-left: 20px">
-            <div style="display: inline;padding-right: 20px;">
-                <a style="font-size: 12px">محمد مبین آقاشاهی</a>
+        <div class="middleHeader">
+            <div class="divUserName">
+                <a>محمد مبین آقاشاهی</a>
             </div>
-            <div style="display: inline;text-align: right;direction: rtl">
+            <div class="divSearch">
                 <input class="inputSearch" type="text" placeholder="جست و جو...">
             </div>
         </div>
     </div>
-    <div class="col-12">
+    <div class="col-12 header">
         <ul class="ulNavigator">
             <li><a href="#home">خانه</a></li>
             <li><a href="#news">اخبار</a></li>
