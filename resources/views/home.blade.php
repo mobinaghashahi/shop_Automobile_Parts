4fcb96f4b2e304e043e4e3efefb01629b5e2b819
unknown <mobinaghashahi13@gmail.com>
2023-03-08 20:49:50 +0330

    change home page and add slideShow in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layout.master')
 
 @section('content')
-    <div class="col-12" style="display: flex;justify-content: center">
-        <img src="/logo/banner.jpg" width="100%" style="">
-    </div>
+@include('layout.slideShow')
+
 @endsection
