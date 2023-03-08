b2fd724c79263f7e86e0a3ace64906b5d64cd3e2
unknown <mobinaghashahi13@gmail.com>
2023-03-08 20:57:45 +0330

    create slideShow.blade.php and include it in home.blade.php

---
+++ resources/views/layout/slideShow.blade.php
@@ -0,0 +1,16 @@
+<div class="academyit1 col-12">
+    <div class="mySlides fade">
+        <div class="academyit2">1 / 3</div>
+        <img src="/logo/banner.jpg" style="width:100%">
+    </div>
+    <div class="mySlides fade">
+        <div class="academyit2">2 / 3</div>
+        <img src="/logo/banner2.jpg" style="width:100%">
+    </div>
+    <div class="mySlides fade">
+        <div class="academyit2">3 / 3</div>
+        <img src="/logo/banner3.jpg" style="width:100%">
+    </div>
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
