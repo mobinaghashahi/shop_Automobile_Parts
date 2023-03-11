a2290b2aa89a31dbccdbe1aaf8b73930fb7f6cbb
unknown <mobinaghashahi13@gmail.com>
2023-03-11 16:06:56 +0330

    create bestSelling file

---
+++ resources/views/home/bestSelling.blade.php
@@ -0,0 +1,97 @@
+<div class="col-12 divAllProduct">
+    <div class="col-12 divTitleBestSeller">
+        <p class="titleBestSeller">
+            محصولات پر فروش
+        </p>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+    <div class="col-2 products">
+        <div class="col-12 productImage">
+            <img src="/prodoucts/1.png" width="100%" height="100%">
+        </div>
+        <div class="col-12 center">
+            <p>لنت ترمز</p>
+            <p>قیمت 200 هزار تومان</p>
+        </div>
+    </div>
+</div>
