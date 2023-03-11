5e7f27ef01b7ead31aab700a3607bb947e42fdd5
unknown <mobinaghashahi13@gmail.com>
2023-03-12 03:17:27 +0330

    change files login and register. create form for post data and add name to inputs. add message validation error

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,29 +3,43 @@
 @section('content')
     <div class="col-12" style="display: flex;justify-content: center">
         <div class="col-3 loginForm" style="padding: 50px 0px 50px 0px">
-            <div class="col-12 divLabelInput">
-                <a>موبایل</a>
-            </div>
-            <div class="col-12" style="display: flex;justify-content: center">
-                <input type="text" class="inputText">
-            </div>
-            <div class="col-12 divLabelInput">
-                <a>رمز عبور</a>
-            </div>
-            <div class="col-12 divInputText" >
-                <input type="text" style="width: 100%;border-radius: 10px;height: 30px;border-color: #ed6a12;box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);">
-            </div>
-            <div class="col-12 divInputSubmit">
-                <input class="inputSubmit" type="submit" value="ثبت نام">
-            </div>
-            <div class="col-12" style="text-align: right;text-align: right;direction: rtl">
-                <div style="text-align: right;direction: rtl;display: inline">
-                    <a href="/signup" style="color: black;text-decoration: none"> حساب کاربری ندارید؟ ثبت نام کنید.</a>
+            @if ($errors->any())
+                @foreach($errors->all() as $error)
+                    <div class="col-12" style="justify-content: center;display: flex">
+                        <div class="col-12">
+                            <p style="color: #ffffff;text-align: center;background-color: #ff2e2e;direction:rtl;line-height: 15px;padding: 10px;border-radius: 10px">{{$error}}</p>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
+            <form method="post" name="enter" action="/login">
+                @csrf
+                <div class="col-12 divLabelInput">
+                    <a>موبایل</a>
                 </div>
-                <div style="text-align: left;direction: rtl;display: inline;float: left">
-                    <a href="#">تغییر گذرواژه</a>
+                <div class="col-12" style="display: flex;justify-content: center">
+                    <input name="phoneNumber" type="text" class="inputText">
                 </div>
-            </div>
+                <div class="col-12 divLabelInput">
+                    <a>رمز عبور</a>
+                </div>
+                <div class="col-12 divInputText">
+                    <input name="password" type="text"
+                           style="width: 100%;border-radius: 10px;height: 30px;border-color: #ed6a12;box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);">
+                </div>
+                <div class="col-12 divInputSubmit">
+                    <input name="enter" class="inputSubmit" type="submit" value="ورود">
+                </div>
+                <div class="col-12" style="text-align: right;text-align: right;direction: rtl">
+                    <div style="text-align: right;direction: rtl;display: inline">
+                        <a href="/register" style="color: black;text-decoration: none"> حساب کاربری ندارید؟ ثبت نام
+                            کنید.</a>
+                    </div>
+                    <div style="text-align: left;direction: rtl;display: inline;float: left">
+                        <a href="#">تغییر گذرواژه</a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
---
+++ resources/views/register.blade.php
@@ -0,0 +1,54 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="col-12" style="display: flex;justify-content: center">
+        <div class="col-3 loginForm" style="padding: 40px 0px 40px 0px">
+            <form method="post" name="enter" action="/register">
+                @csrf
+                @if ($errors->any())
+                    @foreach($errors->all() as $error)
+                        <div class="col-12" style="justify-content: center;display: flex">
+                            <div class="col-12">
+                                <p style="color: #ffffff;text-align: center;background-color: #ff2e2e;direction:rtl;line-height: 15px;padding: 10px;border-radius: 10px">{{$error}}</p>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+                <div class="col-12 divLabelInput">
+                    <a>موبایل</a>
+                </div>
+                <div class="col-12" style="display: flex;justify-content: center">
+                    <input name="phoneNumber" type="text" class="inputText">
+                </div>
+                <div class="col-12 divLabelInput">
+                    <a>نام و نام خانوادگی</a>
+                </div>
+                <div class="col-12 divInputText">
+                    <input name="nameAndFamily" type="text" class="inputText">
+                </div>
+                <div class="col-12 divLabelInput">
+                    <a>رمز عبور</a>
+                </div>
+                <div class="col-12 divInputText">
+                    <input name="password" type="text"
+                           style="width: 100%;border-radius: 10px;height: 30px;border-color: #ed6a12;box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);">
+                </div>
+                <div class="col-12 divLabelInput">
+                    <a>تکرار رمز عبور</a>
+                </div>
+                <div class="col-12 divInputText">
+                    <input name="passwordRepeat" type="text"
+                           style="width: 100%;border-radius: 10px;height: 30px;border-color: #ed6a12;box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);">
+                </div>
+                <div class="col-12 divInputSubmit">
+                    <input name="enter" class="inputSubmit" type="submit" value="ورود">
+                </div>
+                <div class="col-12" style="text-align: right;text-align: right;direction: rtl">
+                    <div style="text-align: right;direction: rtl;display: inline">
+                        <a href="/login" style="color: black;text-decoration: none">از قبل حساب دارید؟</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
