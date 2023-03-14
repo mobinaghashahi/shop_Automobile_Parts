25d27d76046db3022a5671a0a2201cbd0f484c42
unknown <mobinaghashahi13@gmail.com>
2023-03-15 01:12:55 +0330

    create productDetails view

---
+++ resources/views/productDetails.blade.php
@@ -0,0 +1,27 @@
+@extends("layout.master")
+@section('content')
+    <div class="col-12" style="padding: 10px 0px 10px 0px">
+        <div class="col-7">
+            <div class="col-12" style="display: flex;justify-content: center">
+                <img src="prodoucts/1.png" width="60%" height="60%">
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="col-12" style="margin-right: 40px;font-size: 30px">
+                <p style="text-align: right;font-weight: bolder">لنت ترمز ماشین</p>
+            </div>
+            <div class="col-12" style="margin: 0px 40px 0px 20px">
+                <p style="text-align: right">برند: کارماتک</p>
+                <p style="text-align: right">موجودی: 15 عدد</p>
+                <p style="text-align: right">قیمت تکی: 2500 تومان</p>
+                <p style="text-align: right">قیمت عمده: 1000 تومان</p>
+                <p style="text-align: right;direction: rtl">توضیحات: این کالا برای خرید های بالای 100 عدد دارای تخفیف است. و میتوانید آن را به صورت عمده و تکی دریافت کنید.</p>
+            </div>
+            <div class="col-10" style="margin-right: 40px">
+                <input class="inputSubmit" type="submit" value="خرید">
+            </div>
+        </div>
+
+
+    </div>
+@endsection
