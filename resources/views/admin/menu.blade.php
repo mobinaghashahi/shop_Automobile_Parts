6e108f1d3f406521ec81958f3d27308c9685be81
unknown <mobinaghashahi13@gmail.com>
2023-03-16 02:51:57 +0330

    create menu.blade.php and separate menuBar and home page admin.seprate style css and js of main file

---
+++ resources/views/admin/menu.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/styleLogos.css" rel="stylesheet">
+    <link href="/css/styleNavBar.css" rel="stylesheet">
+    <link href="/css/slideShow.css" rel="stylesheet">
+    <link href="/css/RWD.css" rel="stylesheet">
+    <link href="/css/messages.css" rel="stylesheet">
+    <link href="/css/adminPanel.css" rel="stylesheet">
+    <link href="/css/adminPanel.css" rel="java">
+</head>
+<body style="background-color: #eeeeee">
+
+<div class="sidenav">
+    <div style="background-color: #363636;padding: 0px;margin:auto;height: 50px">
+        <p style="font-size: larger;margin: auto;padding-top: 10px;padding-right: 10px">منو</p>
+    </div>
+
+    <div class="col-12" style="background-color: #fed000;padding: 0px;margin:auto;height: 50px;width: 100%">
+        <div class="col-9" >
+            <p style="font-size: larger;margin: auto;padding-top: 10px;padding-right: 10px">مدیریت سایت</p>
+        </div>
+        <div class="col-3" style="padding-top: 15px">
+            <img src="/logo/adminPanel.png" width="30" height="20">
+        </div>
+    </div>
+
+    <button class="dropdown-btn">مدیریت محصولات
+        <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-container">
+        <a href="#">افزودن محصول جدید</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+    </div>
+    <button class="dropdown-btn">مدیریت فروش
+        <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-container">
+        <a href="#">افزودن محصول جدید</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+    </div>
+</div>
