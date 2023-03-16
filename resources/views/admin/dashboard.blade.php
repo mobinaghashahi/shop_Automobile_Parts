bba425789290cbb8f994eb5863254536aaebad38
unknown <mobinaghashahi13@gmail.com>
2023-03-17 02:25:48 +0330

    change format view dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,10 +1,9 @@
-@include('admin.menu')
-<div class="main">
-    <h2>Sidebar Dropdown</h2>
-    <p>Click on the dropdown button to open the dropdown menu inside the side navigationpen the dropdown pen the dropdown pen the dropdown asdasd.</p>
-    <p>This sidebar is of full height (100%) and always shown.</p>
-    <p>Some random text..</p>
-</div>
-<script src="/js/adminPanel.js"></script>
-</body>
-</html>
+@extends('admin.menu')
+@section('content')
+    <div class="main">
+        <h2>Sidebar Dropdown</h2>
+        <p>Click on the dropdown button to open the dropdown menu inside the side navigationpen the dropdown pen the dropdown pen the dropdown asdasd.</p>
+        <p>This sidebar is of full height (100%) and always shown.</p>
+        <p>Some random text..</p>
+    </div>
+@endsection
