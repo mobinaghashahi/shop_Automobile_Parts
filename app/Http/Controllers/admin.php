80f33df5c2050301268631f1950019b372d725bb
unknown <mobinaghashahi13@gmail.com>
2023-03-17 02:24:33 +0330

    create function for show addProduct page

--- app/Http/Controllers/admin.php
+++ app/Http/Controllers/admin.php
@@ -9,4 +9,7 @@ class admin extends Controller
     public function showDashboard(){
         return view('admin.dashboard');
     }
+    public function showAddProduct(){
+        return view('admin.addProduct');
+    }
 }
