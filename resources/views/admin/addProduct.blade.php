72389a2b6c181447f4d307ad4ba45d0c3405caa7
unknown <mobinaghashahi13@gmail.com>
2023-03-17 16:59:43 +0330

    complete addProduct and finished it

--- resources/views/admin/addProduct.blade.php
+++ resources/views/admin/addProduct.blade.php
@@ -1,79 +1,110 @@
 @extends('admin.menu')
 @section('content')
-<div class="main">
-    <div class="col-12" style="text-align: center;padding-top: 15px;">
-        <a style="background-color: #363636;padding: 10px;border-radius: 5px;color: white">افزودن محصول جدید</a>
+    <div class="main">
+        <div class="col-12" style="text-align: center;padding-top: 15px;">
+            <a style="background-color: #363636;padding: 10px;border-radius: 5px;color: white">افزودن محصول جدید</a>
+        </div>
+        <div class="blockOfInputs">
+            @if (\Session::has('msg'))
+                <div class="col-12" style="justify-content: center;display: flex">
+                    <div class="col-3">
+                        <div class="successMessage" style="margin-top: 25px">
+                            {!! \Session::get('msg') !!}
+                        </div>
+                    </div>
+                </div>
+
+            @endif
+            @if ($errors->any())
+                @foreach($errors->all() as $error)
+                    <div class="col-12" style="justify-content: center;display: flex">
+                        <div class="col-3">
+                            <p style="color: #ffffff;text-align: center;background-color: #ff2e2e;direction:rtl;line-height: 15px;padding: 10px;border-radius: 10px">{{$error}}</p>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
+
+            <form method="post" name="enter" enctype="multipart/form-data" action="/admin/addProduct">
+                @csrf
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4">
+                        <input name="name" style="text-align: center" class="inputText" placeholder="نام محصول">
+                    </div>
+                </div>
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4">
+                        <input name="price" style="text-align: center" class="inputText" placeholder="قیمت">
+                    </div>
+                </div>
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4">
+                        <input name="count" style="text-align: center" class="inputText" placeholder="تعداد">
+                    </div>
+                </div>
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4">
+                        <textarea name="description" class="inputText" style="text-align: center"
+                                  placeholder="توضیحات"></textarea>
+                    </div>
+                </div>
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4" style="text-align: center">
+                        <label>برند</label>
+                        <select class="inputText" style="background-color: white" name="brand_id" id="cars">
+                            @foreach ($brand as $index => $brandRows)
+                                <option value="{{$brandRows->id}}">{{$brandRows->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4" style="text-align: center">
+                        <label>نوع ماشین</label>
+                        <select class="inputText" style="background-color: white" name="carType_id" id="cars">
+                            @foreach ($carType as $index => $carTypeRows)
+                                <option value="{{$carTypeRows->id}}">{{$carTypeRows->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4" style="text-align: center">
+                        <label>تخفیف</label>
+                        <select class="inputText" style="background-color: white" name="off_id" id="cars">
+                            @foreach ($off as $index => $offRows)
+                                <option value="{{$offRows->id}}">{{$offRows->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4" style="text-align: center">
+                        <label>دسته بندی</label>
+                        <select class="inputText" style="background-color: white" name="category_id" id="cars">
+                            @foreach ($category as $index => $categoryRows)
+                                <option value="{{$categoryRows->id}}">{{$categoryRows->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
+                    <div class="col-4" style="text-align: center">
+                        <input id="file" type="file" class="form-control @error('file') is-invalid @enderror"
+                               name="file">
+                        @error('file')
+                        <span class="invalid-feedback" role="alert">
+						<strong>{{ $message }}</strong>
+					</span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-12" style="padding-top: 10px;display: flex;justify-content: center">
+                    <div class="col-3">
+                        <input class="inputSubmit" type="submit" value="افزودن">
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="blockOfInputs">
-        <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
-            <div class="col-4">
-                <input style="text-align: center" class="inputText" placeholder="نام محصول">
-            </div>
-        </div>
-        <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
-            <div class="col-4">
-                <input style="text-align: center" class="inputText" placeholder="قیمت">
-            </div>
-        </div>
-        <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
-            <div class="col-4">
-                <input style="text-align: center" class="inputText" placeholder="تعداد">
-            </div>
-        </div>
-        <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
-            <div class="col-4">
-                <textarea class="inputText" style="text-align: center" placeholder="توضیحات"></textarea>
-            </div>
-        </div>
-        <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
-            <div class="col-4" style="text-align: center">
-                <label>برند</label>
-                <select class="inputText" style="background-color: white" name="cars" id="cars">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
-            <div class="col-4" style="text-align: center">
-                <label>نوع ماشین</label>
-                <select class="inputText" style="background-color: white" name="cars" id="cars">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
-            <div class="col-4" style="text-align: center">
-                <label>تخفیف</label>
-                <select class="inputText" style="background-color: white" name="cars" id="cars">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-6 titleTextInput" style="display: flex;justify-content: center">
-            <div class="col-4" style="text-align: center">
-                <label>دسته بندی</label>
-                <select class="inputText" style="background-color: white" name="cars" id="cars">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-12" style="padding-top: 10px;display: flex;justify-content: center">
-            <div class="col-3">
-                <input class="inputSubmit" type="submit" value="افزودن">
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
