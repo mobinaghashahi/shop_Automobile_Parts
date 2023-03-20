517c9ceeba23cae6f8160b9de8a679cc267add08
unknown <mobinaghashahi13@gmail.com>
2023-03-20 17:34:14 +0330

    add route for delete and editPnael view

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,8 @@ Route::prefix('/admin')->middleware(adminMiddleware::class)->group(function () {
 
     Route::get('/addProduct', [admin::class, 'showAddProduct']);
     Route::post('/addProduct', [admin::class, 'addProduct']);
+    Route::get('/editProduct', [admin::class, 'showEditProduct']);
+    Route::get('/deleteProduct/{id}', [admin::class, 'deleteProduct']);
 
     Route::get('/addBrand', [admin::class, 'showAddBrand']);
     Route::post('/addBrand', [admin::class, 'addBrand']);
