fe791ad0f6081ca576bcf74c33e12e63e2cfde7a
unknown <mobinaghashahi13@gmail.com>
2023-03-20 16:10:53 +0330

    change prodouct link to product

--- resources/views/home/latestProducts.blade.php
+++ resources/views/home/latestProducts.blade.php
@@ -6,7 +6,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -15,7 +15,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -24,7 +24,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -33,7 +33,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -42,7 +42,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -51,7 +51,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -60,7 +60,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -69,7 +69,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 divImageProduct">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -78,7 +78,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
@@ -87,7 +87,7 @@
     </div>
     <div class="col-2 products">
         <div class="col-12 productImage">
-            <img class="imageProduct" src="prodoucts/1.png">
+            <img class="imageProduct" src="products/1.png">
         </div>
         <div class="col-12 center">
             <p>لنت ترمز</p>
