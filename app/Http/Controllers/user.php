49f5a4ec89aa7a61d953287b3694f5f10f21a08e
unknown <mobinaghashahi13@gmail.com>
2023-03-30 03:19:20 +0430

    add show details and all products buy

--- app/Http/Controllers/user.php
+++ app/Http/Controllers/user.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Buy;
+use App\Models\Cart;
+use App\Models\Product;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class user extends Controller
 {
     public function showDashboard(){
@@ -14,6 +17,12 @@ class user extends Controller
         return view('user.profile');
     }
     public function showOrders(){
-        return view('user.orders');
+        return view('user.orders',['orders'=>Cart::where('user_id','=',Auth::user()->id)->get()]);
+    }
+    public function showOrderDetails($id){
+        return view('user.oderDetails',['buys'=>Buy::join('products', 'products.id', '=', 'buy.products_id')
+            ->where('cart_id','=',$id)
+            ->select('cart_id as cartId','products.name', 'buy.count as count','buy.price as price')
+            ->get()]);
     }
 }
