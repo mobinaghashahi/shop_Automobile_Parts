307fa61dc7f2a0d7232644fec98be2a02dfa2bf5
unknown <mobinaghashahi13@gmail.com>
2023-03-30 03:32:22 +0430

    add cart Migration;

---
+++ database/migrations/2023_03_02_112136_cart.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("cart", function (Blueprint $table) {
+            $table->id();
+            $table->integer("state");
+            $table->integer("totalPrice");
+            $table->string("paymentCode");
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
