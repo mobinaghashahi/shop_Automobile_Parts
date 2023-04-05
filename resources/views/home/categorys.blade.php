28eab5bc5a07b559ba443850c1647826ccebb01c
unknown <mobinaghashahi13@gmail.com>
2023-04-05 17:52:27 +0430

    change show categorys to owl carousel

--- resources/views/home/categorys.blade.php
+++ resources/views/home/categorys.blade.php
@@ -9,26 +9,26 @@
             @php
                 $count=0;
             @endphp
+            <div class="owl-carousel owl-theme">
             @foreach($products as $product)
                 @if($product->category_id==$category->id)
                     @php
                         $count++
                     @endphp
-                    @if($count==6)
+                    @if($count==20)
                         @break
                     @endif
-
-                    <div class="col-2 products">
-                        <a href="/productDetails/{{$product->id}}" style="color: black">
-                            <div class="col-12 center">
-                                <img class="imageProduct " src="products/{{$product->id}}/1.png">
-                            </div>
-                            <div class="col-12" style="text-align: center">
-                                <p>{{$product->name}}</p>
-                                <p style="direction: rtl"> {{number_format($product->price)}} تومان</p>
-                            </div>
-                        </a>
-                    </div>
+                        <a href="/productDetails/{{$product->id}}" style="color:black"><div class="col-12">
+                                <div class="col-12 center">
+                                    <img class="imageProduct " src="products/{{$product->id}}/1.png">
+                                </div>
+                                <div>
+                                    <p style="text-align: center">{{$product->name}}</p>
+                                </div>
+                                <div>
+                                    <p style="text-align: center"> {{number_format($product->price)}} تومان</p>
+                                </div>
+                            </div></a>
                 @endif
             @endforeach
         </div>
