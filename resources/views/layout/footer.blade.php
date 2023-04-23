adcdc35b2477afa10a64c3d3bc178b1cc50775f8
unknown <mobinaghashahi13@gmail.com>
2023-04-23 19:04:00 +0430

    add ENAMAD logo to footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="col-12 footer" style="color: white;direction: rtl;text-align: right;">
-    <div class="col-4">
+    <div class="col-3">
         <div class="contentFooter">
             <p>اطلاعات تماس</p>
             <p><a style="text-decoration: none;color: white" href="tel:09129231997">تماس: 09129231997</a></p>
@@ -7,17 +7,25 @@
             <p>آدرس:<br>قزوین، بلوار نوروزیان، روبروی بانک ملت، بین البرز ۳۹  </p>
         </div>
     </div>
-    <div class="col-4">
+    <div class="col-3">
         <div class="contentFooter" >
             <p><a href="/aboutUs" style="text-decoration: none;color: white">درباره ما</a></p>
             <p><a href="/contact" style="text-decoration: none;color: white">تماس با ما</a></p>
         </div>
     </div>
-    <div class="col-4">
+    <div class="col-3">
         <div class="contentFooter" style="margin-top: 20px">
             <p>فروشگاه یدکی اصلی</p>
             <a href="https://goo.gl/maps/dzMrSDWcweQtduYi6">
                 <img src="/logo/map.png" width="250" height="200">
+            </a>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="contentFooter" style="margin-top: 20px">
+            <p>نماد و استاندارد ها</p>
+            <a href="https://trustseal.enamad.ir/?id=337118&amp;Code=MqbcszGUlIijqFHhUu7l" target="_blank" rel="noopener">
+                <img id="MqbcszGUlIijqFHhUu7l" src="https://Trustseal.eNamad.ir/logo.aspx?id=337118&amp;Code=MqbcszGUlIijqFHhUu7l" />
             </a>
         </div>
     </div>
