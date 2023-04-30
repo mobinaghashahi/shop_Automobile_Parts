3ee7f9f2551ba26b490859118132af9b8bb0adca
unknown <mobinaghashahi13@gmail.com>
2023-04-30 18:17:07 +0430

    clean code for payment

--- app/Http/Controllers/payment.php
+++ app/Http/Controllers/payment.php
@@ -16,38 +16,11 @@ class payment extends Controller
     public function pay()
     {
         $totalPrice = totalPriceCart();
+        $response = pay($totalPrice);
 
-        $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.zarinpal.com/pg/v4/payment/request.json',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => '{
-  "merchant_id": "91553fab-dac6-4b9f-85b4-2813c98e5bad",
-  "amount": "' . $totalPrice . '",
-  "callback_url": "https://yadakasli.ir/payment/verify",
-  "description": "Transaction description.",
-  "currency": "IRT",
-  "metadata": {
-  }
-}',
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json',
-                'Accept: application/json'
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        
         $authority = json_decode($response)->data->authority;
+
         session(['amount' => $totalPrice]);
         session(['authority' => $authority]);
 
@@ -81,13 +54,10 @@ class payment extends Controller
                 $buy->count = $value;
                 $buy->save();
             }
-
             //پاک کردن سبد خرید
             session()->forget('products');
-
             return redirect()->intended('/user/orders')->with('msg', 'پرداخت با موفقیت انجام شد.');;
         }
-
         return redirect()->intended('/cart/showCart')->with('error', 'در پرداخت مشکلی پیش آمده است. مجددا تلاش کنید.');
 
     }
