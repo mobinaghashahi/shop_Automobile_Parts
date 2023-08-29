6b043c8de7db2ecb7fdd51f67c29d04a982885ce
unknown <mobinaghashahi13@gmail.com>
2023-08-29 04:08:38 +0330

    sort products by new updates

--- app/Http/Controllers/apiProducts.php
+++ app/Http/Controllers/apiProducts.php
@@ -11,13 +11,13 @@ class apiProducts extends Controller
     public function getAllProducts()
     {
         $post_data = array();
-        $products=Product::all();
+        $products=Product::all()->sortByDesc("updated_at");
         foreach ($products as $product) {
             $post_data[] = ['product_id' => $product->id,
                 'page_url' => "https://yadakasli.ir/productDetails/" . $product->id,
                 'price'=>$product->price,
                 'availability'=>$product->availability,
-                'old_price',$product->old_price];
+                'old_price'=>$product->old_price];
         }
         //var_dump($post_data);
         $post_data = json_encode($post_data,JSON_UNESCAPED_SLASHES);
