e1cad86006dd68f4a951a93d74230b06e523b208
unknown <mobinaghashahi13@gmail.com>
2023-09-11 17:26:17 +0330

    delete products count in Details page

--- resources/views/productDetails.blade.php
+++ resources/views/productDetails.blade.php
@@ -36,7 +36,6 @@
                 </div>
                 <div class="col-12 productsDetailsText">
                     <p style="text-align: right"><b>برند:</b> {{$product[0]->brandName}}</p>
-                    <p style="text-align: right"><b>موجودی:</b> {{stock($product[0]->id)}} عدد</p>
                     <p style="text-align: right"><b>قیمت تکی:</b> {{number_format($product[0]->price)}} تومان</p>
                     <p style="text-align: right;direction: rtl">برای اطلاع از قیمت عمده این محصول تماس حاصل نمایید.</p>
                     <p style="text-align: right;direction: rtl;word-wrap: break-word">{{$product[0]->description}}</p>
